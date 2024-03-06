e8cf788cfee28edae8db7692ec302da04db02da0
Luigi Aguirre <luaguirre@eagle.org>
2024-03-06 16:29:47 -0500

    Move locators to their own folders and organize POM

--- e2e/ticketmaster/pages/loginPage.ts
+++ e2e/ticketmaster/pages/loginPage.ts
@@ -1,4 +1,26 @@
 import { BasePage } from "@common/pages/basePage";
+import * as baseConfig from '../../../conf/base.config.json';
+import * as locators from '../resources/locators/loginPageLocators.json';
 
-export class LoginPage extends BasePage{
+class LoginPage extends BasePage{
+
+    private emailTextFieldLocator: string = locators.emailTextField[baseConfig.OS];
+    private passwordTextFieldLocator: string = locators.passwordTextField[baseConfig.OS];
+    private signInButtonLocator: string = locators.signInButton[baseConfig.OS];
+
+
+    public get emailTextField(){ 
+        return this.getElement(this.emailTextFieldLocator);
+    }
+
+    public get passwordTextField(){ 
+        return this.getElement(this.passwordTextFieldLocator);
+    }
+
+    public get signInButton(){ 
+        return this.getElement(this.signInButtonLocator);
+    }
+
 }
+
+export const loginPage = new LoginPage();
--- e2e/ticketmaster/pages/myAccountPage.ts
+++ e2e/ticketmaster/pages/myAccountPage.ts
@@ -1,4 +1,19 @@
 import { BasePage } from "@common/pages/basePage";
+import * as baseConfig from '../../../conf/base.config.json';
+import * as locators from '../resources/locators/myAccountPageLocators.json';
 
-export class MyAccountPage extends BasePage{
+class MyAccountPage extends BasePage{
+    private signOutButtonLocator: string = locators.signOutButton[baseConfig.OS];
+    private headerTextLabelLocator: string = locators.headerTextLabel[baseConfig.OS];
+
+
+    public get signOutButton(){ 
+        return this.getElement(this.signOutButtonLocator);
+    }
+
+    public get headerTextLabel(){ 
+        return this.getElement(this.headerTextLabelLocator);
+    }
 }
+
+export const myAccountPage = new MyAccountPage();
